d87a694a6e5b714463f07a245f278f5fbf674724
Berkan Atpinar <batpnar@gmail.com>
2022-05-04 16:57:26 +0300

    if esle added

---
+++ if-else-if/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time>=6 && time<11)
+                System.Console.WriteLine("Günaydın");
+            else if(time <= 18)
+                System.Console.WriteLine("İyi Günler");
+            else
+                System.Console.WriteLine("İyi geceler");
+
+                string sonuc = time<= 18 ? "İyi Günler" : "İyi Geceler";
+                sonuc = time >= 6 && time<11 ? "Günaydın" : time<= 18 ? "iyi günler" : "iyi geceler";
+                System.Console.WriteLine(sonuc);
+        }
+    }
+}
