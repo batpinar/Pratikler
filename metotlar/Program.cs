55de471f3d86b432a3fa77be4332e403bfca9bcf
Berkan Atpinar <batpnar@gmail.com>
2022-05-08 13:09:08 +0300

    metotlar eklendi

---
+++ metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim_belirteci geri_donuctipi metot_adi(parametreListesi/arguman)
+            // {
+            //     //komutlar
+                    //return
+            // }
+
+            int a = 2;
+            int b = 4;
+
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.ekranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.arttırVeTopla(ref a,ref b);
+            ornek.ekranaYazdir(Convert.ToString(sonuc2));
+            ornek.ekranaYazdir(Convert.ToString(a+b));
+        }
+
+        static int Topla(int deger1, int deger2){
+            return  (deger1 + deger2);
+
+        }
+    }
+
+    class Metotlar{
+       public void ekranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+
+        public int arttırVeTopla(ref int deger1,ref int deger2){
+            deger1++;
+            deger2++;
+            return deger1 + deger2;
+        }
+    }
+}
