5d834bfd1d769ff799d50d331c5b2178b3505206
batpinar <batpnar@gmail.com>
2022-05-25 16:07:20 +0300

    arayuzler(interface) ornek

---
+++ arayuzler-ornek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace arayuzler_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Focus focus = new Focus();
+           Console.WriteLine(focus.HangiMarkaninAraci().ToString());
+           Console.WriteLine(focus.KacTekerlektenOlusur());
+           Console.WriteLine(focus.StandartrengiNe().ToString());
+
+           Civic civic = new Civic();
+           Console.WriteLine(civic.HangiMarkaninAraci().ToString());
+           Console.WriteLine(civic.KacTekerlektenOlusur());
+           Console.WriteLine(civic.StandartrengiNe().ToString());
+
+
+           
+        }
+    }
+}
